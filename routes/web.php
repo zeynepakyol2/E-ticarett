68172e9eadefd7dd731a734ad48a8ff4aff8d176
zeynepakyol1 <112568213+zeynepakyol2@users.noreply.github.com>
2024-07-08 18:54:08 +0300

    düzenleme1

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','AnasayfaController@index')->name('anasayfa');
 Route::get('/merhaba',function (){
     return "merhaba";
 });
