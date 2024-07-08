de48eba07ff69ff00282ae8b7fd674c75e91c5f7
zeynepakyol1 <112568213+zeynepakyol2@users.noreply.github.com>
2024-07-08 16:35:01 +0300

    ilk yükleme

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/merhaba',function (){
+    return "merhaba";
+});
+Route::get('api/v1/merhaba',function(){
+    return ['mesaj'=>'merhaba'];
+});
+Route::get('urun/{urunadi}/{id?}',function ($urunadi,$id=4){
+    return "urun adı: $id $urunadi";
+})->name('urun_detay'); //name fonksiyonu ile bu route a erişebilriiz
+Route::get('/kampanya',function (){
+    return redirect()->route('urun_detay',['urunadi'=>'elma','id'=>5]);
+});
